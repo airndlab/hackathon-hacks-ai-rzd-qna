f6208ba37324e7017a2721ed9fec702a3973af38
gravitaliy <gravitaliy@mail.ru>
2024-10-13 10:01:03 +0300

    Added faq, quide

--- bot/config.js
+++ bot/config.js
@@ -4,6 +4,8 @@ const config = {
   botToken: process.env.BOT_TOKEN,
   qnaUrl: process.env.QNA_URL,
   messagesPath: process.env.BOT_MESSAGES_FILE_PATH,
+  faqPath: process.env.FAQ_FILE_PATH,
+  quideUrl: process.env.QUIDE_URL,
 };
 
 module.exports = config;
